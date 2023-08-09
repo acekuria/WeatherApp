2a0d239edeb192d9378569a9cc1c5240e0394012
AlvinKuria <alvinkuria18@gmail.com>
2023-08-09 18:09:17 +0100

    Fetch Weather API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Im working');
+async function getWeather(location) {
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=f9bc59283a7b47699ee150824230708&q=${location}&aqi=no
+      `,
+      {
+        mode: 'cors',
+      },
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+getWeather('Kenya');
